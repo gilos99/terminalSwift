c1e554022298901e0ed76848078b3c81a9f44605
JanGil <jangil@JanGils-Mac.local>
2017-09-23 05:32:20 -0700

    double abs

--- main.swift
+++ main.swift
@@ -7,23 +7,43 @@
 //
 
 import Foundation
-var Imie : String = "Jan"
-var Nazwisko : String = "Gil"
-var liczba : Int = 20
-func powitanie(_ imie : String,_ nazwisko : String)
+var array1 : [Double] = [1.1,2.2,3.3]
+let number : Double = -4
+func arrayMin(array: [Double])-> Double
 {
-print ("Witaj \(imie) \(nazwisko)")
-}
-func liczenie(_ liczba1 : Int) -> Int
-{
+    var min : Double = 0
+    for (index,element) in array.enumerated()
+    {
+        if index==0
+        {
+            min = element
+        }
+    }
+    return min
     
-    return liczba1 + 10
 }
-powitanie(Imie, Nazwisko)
-var wynik = liczenie(liczba)
-
-print (wynik)
-
-
-
-
+func arrayMax(array : [Double])-> Double
+{
+    return array[array.count-1]
+}
+func arrayMaxMin(array : [Double])->(Double, Double)
+{
+   
+    return (array[0] , array[array.count-1])
+    
+}
+func findAbs (liczba : Double?) -> Double?
+{
+    guard let liczba = liczba else { return nil }
+    
+    return liczba < 0 ? -liczba : liczba
+    
+//    if let liczba = liczba, liczba < 0 {
+//      return -liczba
+//    }
+//    return liczba
+}
+print (findAbs(liczba : number)!)
+print (arrayMaxMin(array: array1))
+print(arrayMin(array : array1))
+print(arrayMax(array : array1))
